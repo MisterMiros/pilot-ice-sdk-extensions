c3ae41ace8bc4ab2d6664f4f80d5e77a6b4a2f09
bogatirev.m <bogatirev.m@akvnzm.ru>
2016-10-12 13:33:19 +0500

    Добавляет ITaskBuilderExtensions

---
+++ Ascon.Pilot.SDK.Extensions/ITaskBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ascon.Pilot.SDK.Extensions
+{
+    public static class ITaskBuilderExtensions
+    {
+        public static ITaskBuilder AddInitiatorAttachment(this ITaskBuilder builder, Guid id)
+        {
+            var attachments = builder.TaskObject.InitiatorAttachments.ToList();
+            if (attachments.Contains(id)) { return builder; }
+            attachments.Add(id);
+            return builder.SetInitiatorAttachments(attachments);
+        }
+
+        public static ITaskBuilder AddExecutorAttachment(this ITaskBuilder builder, Guid id)
+        {
+            var attachments = builder.TaskObject.ExecutorAttachments.ToList();
+            if (attachments.Contains(id)) { return builder; }
+            attachments.Add(id);
+            return builder.SetExecutorAttachments(attachments);
+        }
+    }
+}
