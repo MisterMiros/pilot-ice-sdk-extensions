ae4b5d5193a4d794040a4301be7f44d49b43a2f9
bogatirev.m <bogatirev.m@akvnzm.ru>
2016-10-12 14:10:59 +0500

    Исправления

--- Ascon.Pilot.SDK.Extensions/ITaskBuilderExtensions.cs
+++ Ascon.Pilot.SDK.Extensions/ITaskBuilderExtensions.cs
@@ -9,17 +9,33 @@ namespace Ascon.Pilot.SDK.Extensions
     {
         public static ITaskBuilder AddInitiatorAttachment(this ITaskBuilder builder, Guid id)
         {
-            var attachments = builder.TaskObject.InitiatorAttachments.ToList();
-            if (attachments.Contains(id)) { return builder; }
-            attachments.Add(id);
+            IList<Guid> attachments;
+            if (builder.TaskObject.InitiatorAttachments != null)
+            {
+               attachments = builder.TaskObject.InitiatorAttachments.ToList();
+               if (attachments.Contains(id)) { return builder; }
+                attachments.Add(id);
+            }
+            else
+            {
+                attachments = new Guid[] { id };
+            }
             return builder.SetInitiatorAttachments(attachments);
         }
 
         public static ITaskBuilder AddExecutorAttachment(this ITaskBuilder builder, Guid id)
         {
-            var attachments = builder.TaskObject.ExecutorAttachments.ToList();
-            if (attachments.Contains(id)) { return builder; }
-            attachments.Add(id);
+            IList<Guid> attachments;
+            if (builder.TaskObject.ExecutorAttachments != null)
+            {
+                attachments = builder.TaskObject.ExecutorAttachments.ToList();
+                if (attachments.Contains(id)) { return builder; }
+                attachments.Add(id);
+            }
+            else
+            {
+                attachments = new Guid[] { id };
+            }
             return builder.SetExecutorAttachments(attachments);
         }
     }
