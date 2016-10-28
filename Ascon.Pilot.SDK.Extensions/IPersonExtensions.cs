6ddb33963823dab138905cfff33757477b0c07d0
bogatirev.m <bogatirev.m@akvnzm.ru>
2016-10-28 09:04:57 +0500

    Добавляет проверку наличия должности у IPerson

---
+++ Ascon.Pilot.SDK.Extensions/IPersonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ascon.Pilot.SDK.Extensions
+{
+    public static class IPersonExtensions
+    {
+        public static bool HasPositionWithName(this IPerson person, string position)
+        {
+            return (from orgUnit in (from pos in person.Positions
+                                     select pos.GetOrgUnit())
+                    where orgUnit.Title == position
+                    select orgUnit).Any();
+        }
+    }
+}
