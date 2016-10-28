d731c0da5bd5fa0729bcbb56856f3a51c2f1c6e8
bogatirev.m <bogatirev.m@akvnzm.ru>
2016-10-28 14:02:30 +0500

    Добавляет получение вложений и предыдущих версий задачи

--- Ascon.Pilot.SDK.Extensions/ITaskObjectExtensions.cs
+++ Ascon.Pilot.SDK.Extensions/ITaskObjectExtensions.cs
@@ -12,5 +12,28 @@ namespace Ascon.Pilot.SDK.Extensions
         {
             return Extensions.Repository.Get<IDataObject>(task.Id);
         }
+
+        public static IEnumerable<IDataObject> GetInitiatorAttachments(this ITaskObject task)
+        {
+            return Extensions.Repository.Get<IDataObject>(task.InitiatorAttachments);
+        }
+
+        public static IEnumerable<IDataObject> GetExecutorAttachments(this ITaskObject task)
+        {
+            return Extensions.Repository.Get<IDataObject>(task.ExecutorAttachments);
+        }
+
+        public static IEnumerable<ITaskObject> GetPreviousVersions(this ITaskObject task)
+        {
+            IDataObject dataObject = task.ToDataObject();
+            foreach (var child in dataObject.GetChildren())
+            {
+                if (child.Type.Name == SystemTypeNames.TASK)
+                {
+                    var childTask = child.ToTask();
+                    if (childTask.IsVersion) { yield return childTask; }
+                }
+            }
+        }
     }
 }
